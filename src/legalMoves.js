2a348a22e598a2cc008e2686da6c445f2cb769c4
adamromero <adamromero89@gmail.com>
2022-07-13 19:28:19 -0700

    Fix rook legal moves

--- src/legalMoves.js
+++ src/legalMoves.js
@@ -1,3 +1,10 @@
+import {
+   isMoveOpen,
+   getOpenMoves,
+   getUnblockedMoves,
+   getInBoundMoves,
+} from "./openMoves.js";
+
 const pawnLegalMoves = (piece) => {
    const x = piece.position[0];
    const y = piece.position[1];
@@ -8,7 +15,9 @@ const pawnLegalMoves = (piece) => {
             [x - 2, y],
          ];
       } else {
-         return [[x - 1, y]];
+         if (isMoveOpen([x - 1, y])) {
+            return [[x - 1, y]];
+         }
       }
    } else {
       if (x === 1) {
@@ -22,23 +31,62 @@ const pawnLegalMoves = (piece) => {
    }
 };
 
-const bishopLegalMoves = (piece) => {
+const rookLegalMoves = (piece) => {
    let moves = [];
-   console.log(piece);
+   const x = piece.position[0];
+   const y = piece.position[1];
+
    for (let i = 1; i < 8; i++) {
-      if (piece.color === "white") {
-         moves.push(piece.position - i * 9);
-         moves.push(piece.position - i * 7);
-         moves.push(piece.position + i * 7);
-         moves.push(piece.position + i * 9);
-      } else {
-         moves.push(piece.position + i * 9);
-         moves.push(piece.position + i * 7);
-         moves.push(piece.position - i * 7);
-         moves.push(piece.position - i * 9);
-      }
+      moves.push([x + i, y]);
+      moves.push([x - i, y]);
    }
+   for (let i = 1; i < 8; i++) {
+      moves.push([x, y + i]);
+      moves.push([x, y - i]);
+   }
+   moves = getInBoundMoves(moves);
+   moves = getUnblockedMoves(moves);
    return moves;
 };
 
-export { pawnLegalMoves, bishopLegalMoves };
+const bishopLegalMoves = (piece) => {
+   let moves = [];
+   const x = piece.position[0];
+   const y = piece.position[1];
+   for (let i = 1; i < 8; i++) {
+      if (piece.color === "white") {
+         moves.push([x - i, y - i]);
+         moves.push([x - i, y + i]);
+         moves.push([x + i, y - i]);
+         moves.push([x + i, y + i]);
+      } else {
+         moves.push([x + i, y + i]);
+         moves.push([x + i, y - i]);
+         moves.push([x - i, y + i]);
+         moves.push([x - i, y - i]);
+      }
+   }
+   moves = getInBoundMoves(moves);
+   moves = getOpenMoves(moves);
+   moves = getUnblockedMoves(moves);
+   return moves;
+};
+
+const knightLegalMoves = (piece) => {
+   let moves = [];
+   const x = piece.position[0];
+   const y = piece.position[1];
+   moves.push([x - 1, y - 2]);
+   moves.push([x - 2, y - 1]);
+   moves.push([x - 2, y + 1]);
+   moves.push([x - 1, y + 2]);
+   moves.push([x + 1, y + 2]);
+   moves.push([x + 2, y + 1]);
+   moves.push([x + 2, y - 1]);
+   moves.push([x + 1, y - 2]);
+   moves = getInBoundMoves(moves);
+   moves = getOpenMoves(moves);
+   return moves;
+};
+
+export { pawnLegalMoves, rookLegalMoves, bishopLegalMoves, knightLegalMoves };
---
+++ src/openMoves.js
@@ -0,0 +1,44 @@
+import boardMap from "./boardMap.js";
+
+export const isMoveOpen = (move) => {
+   const x = move[0];
+   const y = move[1];
+   return boardMap[x][y] === 0;
+};
+
+export const getOpenMoves = (moves) => {
+   let openMoves = [];
+   for (let i = 0; i < moves.length; i++) {
+      if (isMoveOpen(moves[i])) {
+         openMoves.push(moves[i]);
+      }
+   }
+   console.log(openMoves);
+   return openMoves;
+};
+
+export const getUnblockedMoves = (moves) => {
+   let openMoves = [];
+   for (let i = 0; i < moves.length; i++) {
+      if (isMoveOpen(moves[i])) {
+         openMoves.push(moves[i]);
+      } else {
+         //break;
+      }
+   }
+
+   return openMoves;
+};
+
+export const getInBoundMoves = (moves) => {
+   let inBoundMoves = [];
+   for (let i = 0; i < moves.length; i++) {
+      const x = moves[i][0];
+      const y = moves[i][1];
+      if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+         inBoundMoves.push(moves[i]);
+      }
+   }
+
+   return inBoundMoves;
+};
